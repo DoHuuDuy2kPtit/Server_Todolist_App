ef23b5e302998888d6bb66e80cf135eb05943ceb
Duy DH <duy.do@amela.vn>
2022-04-25 15:41:28 +0700

    Add knexfile.js

---
+++ knexfile.js
@@ -0,0 +1,25 @@
+const dotenv = require('dotenv');
+
+dotenv.config({ path: '.env' });
+
+module.exports = {
+  client: process.env.DB_CLIENT,
+  connection: {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_DATABASE,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+  },
+  pool: {
+    min: 2,
+    max: 10,
+  },
+  migrations: {
+    tableName: 'migrations',
+    directory: './database/migrations',
+  },
+  seeds: {
+    directory: './database/seeds',
+  },
+};
